e89f97be588e4b44ccdfd8a7e895077f59b28b33
VJalili <jalili.vahid@gmail.com>
2017-06-18 01:41:52 -0700

    Initial Commit.

---
+++ SimDataGen/SimDataGen/Components/IntervalBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VahidJalili.Di4.SimDataGen
+{
+    internal class IntervalBase : IComparable<IntervalBase>
+    {
+        public IntervalBase()
+        { }
+
+        public IntervalBase(int left, int right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        public IntervalBase(IntervalBase interval)
+        {
+            left = interval.left;
+            right = interval.right;
+        }
+
+        public int left { set; get; }
+        public int right { set; get; }
+
+        public void Intersect(IntervalBase interval)
+        {
+            left = Math.Max(left, interval.left);
+            right = Math.Min(right, interval.right);
+        }
+
+        public int CompareTo(IntervalBase other)
+        {
+            if (right <= other.left) return -1;
+            if (left >= other.right) return 1;
+            return 0;
+        }
+    }
+}
---
+++ SimDataGen/SimDataGen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VahidJalili.Di4.SimDataGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var simData = new Di4DataSim(Environment.CurrentDirectory);
+            simData.Generate(
+                sampleCount: 20,
+                maxICount: 1000,
+                chrCount: 23,
+                kDis: new ErlangDistribution(1, 20),
+                lambdaDis: new ErlangDistribution(1, 2),
+                fileSizeProb: 5);
+        }
+    }
+}
